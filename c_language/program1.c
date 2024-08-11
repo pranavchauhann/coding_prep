878916ac5f4f0fd40344b18dcfee3e583640c620
Pranav Chauhan <51231197+pranavchauhann@users.noreply.github.com>
2024-08-11 05:06:28 +0000

    c programs

---
+++ c_language/program1.c
@@ -0,0 +1,9 @@
+// function, pointer, structure and Union
+
+// A function in c is a set of statement that when called perform some specific task.
+
+// Function calling is always a overhead in a c language ie it require more computing power and bandwidth.
+
+// there are three aspect of a funtion in a c.  1. function declaration     2. function call    3. function defination
+
+// A function in c must be declared globally before calling it
